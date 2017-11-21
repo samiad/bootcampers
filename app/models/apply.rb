b230360dbe011544e0920c021c1c609457a08768
Philippe Despreaux <despreaux.philippe@wanadoo.fr>
2017-11-21 19:03:12 +0100

    bootcamper::missions#index 50% done controller, view, css

--- app/models/apply.rb
+++ app/models/apply.rb
@@ -2,4 +2,7 @@ class Apply < ApplicationRecord
   belongs_to :user
   belongs_to :mission
 
+  scope :pending, -> { where(accepted_at == nil && declined_at == nil) }
+  # scope :finished, -> { where(self.project.delivered_at !== nil)}
+  # scope :ongoing, -> { where(pending: false && finished: false)}
 end
