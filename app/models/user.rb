afe746bd3b6de0e8df32987fc87bb3d2cc0dc717
Samia Dahmouni <dsamia@live.fr>
2017-11-22 15:02:56 +0100

    added cloudinary and attachinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
   has_one :company
   has_many :missions, through: :applies
 
+  has_attachment :photo
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :bio, presence: true, length: {minimum: 20}
