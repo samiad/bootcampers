ca1f46f3fde0e882882edebacdeac03552a1de6c
Valentin Ferron <ferron.vale@gmail.com>
2017-11-27 12:40:51 +0100

    form in order to create project

--- app/controllers/company/projects_controller.rb
+++ app/controllers/company/projects_controller.rb
@@ -7,4 +7,31 @@ class Company::ProjectsController < ApplicationController
   def Show
 
   end
+
+  def new
+    byebug
+    @company = Company.new
+    @company.projects.build
+  end
+
+  def create
+    @company = Company.new(company_params)
+    @company.save
+  #   @company.user = current_user
+  #   if @company.save
+  #     redirect_to company_path(@company)
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  private
+
+  def set_company
+    @company = Project.find(params[:id])
+  end
+
+  def company_params
+    params.require(:company).permit(:name, :siret, :vat_number, :address, :activity_code, :website, project: Project.allowed_params)
+  end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,21 +1,41 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: :show
 
-def index
+  def index
+    @projects = current_user.projects
+  end
 
-end
+  def show
+  end
 
-def show
+  def new
+    @project = Project.new
+    @company = current_user.company || Company.new
+  end
 
-end
+  def create
+    @company = current_user.company || current_user.build_company(company_params)
+    @project = @company.projects.new(project_params)
 
-def new
-  @project = Project.new
-  # render layout: false
-end
+    if @company.save && @project.save
+      redirect_to root_path, notice: "YOUPI"
+    else
+      # byebug
+      render :new
+    end
+  end
 
-def create
+  private
 
-end
+  def set_project
+    @project = Project.find(params[:id])
+  end
 
+  def company_params
+    params.require(:project).require(:company).permit(:name, :siret, :vat_number, :address, :activity_code, :website)
+  end
 
+  def project_params
+    params.require(:project).permit(:request_title, :request_description, :budget, :request_delivery_at)
+  end
 end
