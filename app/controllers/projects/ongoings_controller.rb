3e445520dddfdf07ba17a21d94441be9fdc9f40c
Philippe Despreaux <despreaux.philippe@wanadoo.fr>
2017-11-28 17:07:37 +0100

    missions created ok with skills

--- app/controllers/projects/ongoings_controller.rb
+++ app/controllers/projects/ongoings_controller.rb
@@ -5,9 +5,7 @@ class Projects::OngoingsController < ApplicationController
   end
 
   def create
-    mp = mission_params
-    mp[:agreed_delivery_at] = Date::strptime(mp[:agreed_delivery_at],"%Y-%m-%d")
-    if @project.update(mp)
+    if @project.update(mission_params)
       redirect_to projects_path
     else
       flash[:error] = "Désolé, cela n'a pas fonctionné"
@@ -19,7 +17,7 @@ class Projects::OngoingsController < ApplicationController
     @project = Project.find(params[:project_id])
   end
   def mission_params
-    params.require(:project).permit(:title, :description, :price, :category, :agreed_delivery_at, missions_attributes: [:id, :title, :description, :price, :_destroy]).merge(scoped_at: DateTime.now)
+    params.require(:project).permit(:title, :description, :price, :category, :agreed_delivery_at, missions_attributes: [:id, :title, :description, :price, :_destroy, skill_ids: []]).merge(scoped_at: DateTime.now)
   end
 
 end
