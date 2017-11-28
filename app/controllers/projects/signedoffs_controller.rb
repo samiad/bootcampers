2d0ff1b95cee9029b3e6f4489c1b6835d4a1385e
Valentin Ferron <ferron.vale@gmail.com>
2017-11-28 19:45:39 +0100

    update pas bon code

--- app/controllers/projects/signedoffs_controller.rb
+++ app/controllers/projects/signedoffs_controller.rb
@@ -1,6 +1,10 @@
 class Projects::SignedoffsController < ApplicationController
 
 def create
+  project = Project.find(params[:project_id])
+  order  = Order.create!(teddy_sku: teddy.sku, amount: teddy.price, state: 'pending')
+
+  redirect_to new_order_payment_path(order)
 
 end
 
