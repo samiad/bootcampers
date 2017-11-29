1596e21875031433c0ca9dd96830405e862510bb
Philippe Despreaux <despreaux.philippe@wanadoo.fr>
2017-11-29 20:25:57 +0100

    validates commented in all models

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -6,9 +6,9 @@ class Mission < ApplicationRecord
   has_many :mission_skills
   has_many :skills, through: :mission_skills
 
-  validates :title, presence: true
-  validates :description, presence: true, length: {minimum: 20}
-  validates :price, presence: true
+  # validates :title, presence: true
+  # validates :description, presence: true, length: {minimum: 20}
+  # validates :price, presence: true
 
 
 
